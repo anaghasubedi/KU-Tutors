51fffd7ac08f88f9a9feffbe4f2a2e86153481df
Anagha Subedi <167497533+anaghasubedi@users.noreply.github.com>
2025-12-04 17:22:50 +0545

    Updated the models

--- Django/kututors/api/models.py
+++ Django/kututors/api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Tutor(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    contact = models.CharField(max_length=10,null=True,blank=False)
     subject = models.CharField(max_length=50)
     available = models.BooleanField(default=True)
 
@@ -13,11 +14,11 @@ class Tutor(models.Model):
 class Tutee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    grade = models.CharField(max_length=20)
-    subjects_needed = models.CharField(max_length=100)
+    contact = models.CharField(max_length=10,null=True,blank=False)
+    semester = models.CharField(max_length=20)
 
     def __str__(self):
-        return f"{self.name} - {self.grade}"
+        return f"{self.name} - {self.semester}"
 
 
 class Session(models.Model):
@@ -25,7 +26,6 @@ class Session(models.Model):
     tutee = models.ForeignKey(Tutee, on_delete=models.CASCADE, related_name='sessions')
     date = models.DateField()
     time = models.TimeField()
-    duration_hours = models.FloatField()
     completed = models.BooleanField(default=False)
 
     def __str__(self):
