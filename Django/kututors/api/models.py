7812be2783e5f90cb89dfd0b110b5d3d67679cd4
Anagha Subedi <167497533+anaghasubedi@users.noreply.github.com>
2025-12-05 18:37:10 +0545

    Changes to fields

--- Django/kututors/api/models.py
+++ Django/kututors/api/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 
 class Tutor(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, primary_key=True)
     email = models.EmailField(unique=True)
     contact = models.CharField(max_length=10,null=True,blank=False)
     subject = models.CharField(max_length=50)
+    semester = models.CharField(max_length=20,default="Unknown")
+    subject = models.CharField(max_length=50, default="Unknown")
+    subjectcode = models.CharField(max_length=10, default="Unknown")
     available = models.BooleanField(default=True)
+    accountnumber =models.CharField(max_length=20, default="Not Provided")
+    bankqr = models.ImageField(upload_to='Tutor_profile/', default="Not Provided")
 
     def __str__(self):
         return f"{self.name} - {self.subject}"
 
 
 class Tutee(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, primary_key=True)
     email = models.EmailField(unique=True)
     contact = models.CharField(max_length=10,null=True,blank=False)
     semester = models.CharField(max_length=20)
+    subjectreqd = models.CharField(max_length=50,default="Unknown")
 
     def __str__(self):
         return f"{self.name} - {self.semester}"
